72105b2e51f6cab84285989b3bcf01392c0ab1bc
BANO.notIT <bano.notit@yandex.ru>
2022-01-14 00:33:10 +0300

    feat: implement new models

---
+++ src/models/Application.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { Event } from './Event'
+
+@Entity()
+export class Application {
+  @PrimaryGeneratedColumn('increment')
+  id: number
+
+  @Column()
+  tgId: number
+
+  @Column()
+  createdAt: Date
+  @Column()
+  firstname: string
+  @Column()
+  midname: string
+  @Column()
+  surname: string
+
+  @Column({ default: false })
+  approved: boolean
+
+  @ManyToOne(() => Event, (event) => event.applications)
+  event: Event
+
+  represent() {
+    return `${this.firstname} ${this.midname} ${this.surname}`
+  }
+}
---
+++ src/models/Event.ts
@@ -0,0 +1,46 @@
+import { Application } from './Application'
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
+
+export enum EventState {
+  open = 'open',
+  additional = 'additional',
+  closed = 'closed'
+}
+
+@Entity()
+export class Event {
+  @PrimaryGeneratedColumn('increment')
+  id: number
+
+  @Column({ type: 'timestamptz' })
+  createdAt: Date
+
+  @Column({ type: 'varchar' })
+  status: EventState
+
+  @Column({ type: 'date' })
+  date: Date
+  @Column({ type: 'time' })
+  start: Date
+  @Column({ type: 'time' })
+  end: Date
+
+  @Column({ type: 'text' })
+  description: string
+
+  @OneToMany(() => Application, (application) => application.event)
+  applications: Application[]
+
+  represent() {
+    let repr = `${this.date} ${this.start}-${this.end}`
+    switch (this.status) {
+      case EventState.additional:
+        repr += ` (добор)`
+        break
+      case EventState.closed:
+        repr += ` (закрыт)`
+    }
+
+    return repr
+  }
+}
