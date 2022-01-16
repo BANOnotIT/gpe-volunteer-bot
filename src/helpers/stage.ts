d06a30a97f7c346131d4ef1ced521aa1bfd6f69e
BANO.notIT <bano.notit@yandex.ru>
2022-01-16 17:37:53 +0300

    feat: log stage setup

--- src/helpers/stage.ts
+++ src/helpers/stage.ts
@@ -4,9 +4,11 @@ import main from '../scenes/main'
 import { PEContext } from '../types/custom-context'
 import { RegisterScene } from '../scenes/RegisterScene'
 import { UserMainScene } from '../scenes/UserMainScene'
+const debug = require('debug')('bot:stage')
 
 export function setupStage(bot: Telegraf<PEContext>) {
   const stage = new Scenes.Stage<PEContext>([main, RegisterScene, UserMainScene], { default: SCENE.DEFAULT })
 
   bot.use(stage.middleware())
+  debug('Stage set')
 }
