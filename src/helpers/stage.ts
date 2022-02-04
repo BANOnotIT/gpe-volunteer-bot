2c8f656b753cadc13148330038f91c7d56c41b8b
BANO.notIT <bano.notit@yandex.ru>
2022-02-04 21:09:09 +0300

    feat: enable new scenes

--- src/helpers/stage.ts
+++ src/helpers/stage.ts
@@ -6,12 +6,17 @@ import { RegisterScene } from '../scenes/RegisterScene'
 import { UserMainScene } from '../scenes/UserMainScene'
 import { EventCreationScene } from '../scenes/EventCreationScene'
 import { AdminMainScene } from '../scenes/AdminMainScene'
+import { EventEditScene } from '../scenes/EventEditScene'
+import { MembersListScene } from '../scenes/MembersListScene'
 const debug = require('debug')('bot:stage')
 
 export function setupStage(bot: Telegraf<PEContext>) {
-  const stage = new Scenes.Stage<PEContext>([main, RegisterScene, UserMainScene, EventCreationScene, AdminMainScene], {
-    default: SCENE.DEFAULT
-  })
+  const stage = new Scenes.Stage<PEContext>(
+    [main, RegisterScene, UserMainScene, EventEditScene, EventCreationScene, AdminMainScene, MembersListScene],
+    {
+      default: SCENE.DEFAULT
+    }
+  )
 
   bot.use(stage.middleware())
   debug('Stage set')
