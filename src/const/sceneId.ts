791bacc2a21e12c996f05e6b1597e38b0f2e17d4
BANO.notIT <bano.notit@yandex.ru>
2022-02-04 21:08:56 +0300

    feat: implement members list

--- src/const/sceneId.ts
+++ src/const/sceneId.ts
@@ -4,5 +4,6 @@ export enum SCENE {
   ADMIN_MAIN = 'admin_main',
   REGISTER_ON_EVENT = 'register_on_event',
   CREATE_EVENT = 'create_event',
-  EDIT_EVENT = 'edit_event'
+  EDIT_EVENT = 'edit_event',
+  LIST_USERS = 'list_users'
 }
---
+++ src/scenes/MembersListScene.ts
@@ -0,0 +1,45 @@
+import phrases from '../helpers/strings'
+import { Composer, Markup, Scenes } from 'telegraf'
+import { Event } from '../models/Event'
+import { SCENE } from '../const/sceneId'
+import { PEContext } from '../types/custom-context'
+import { getRepository } from 'typeorm'
+import { Application } from '../models/Application'
+
+export const MembersListScene = new Scenes.WizardScene<PEContext>(
+  SCENE.LIST_USERS,
+
+  async (ctx) => {
+    const eventRepo = getRepository(Event)
+    const events = await eventRepo.createQueryBuilder('event').addOrderBy('event.date', 'ASC').getMany()
+
+    const eventButtons = events.map((event) => [Markup.button.callback(event.represent(), `event#${event.id}`)])
+    await ctx.reply(phrases.eventEdit.choseEvent(), Markup.inlineKeyboard(eventButtons))
+
+    ctx.scene.session.event = {}
+
+    return ctx.wizard.next()
+  },
+
+  Composer.action<PEContext>(/^event#(\d+)$/, async (ctx) => {
+    let eventId = Number(ctx.match[1])
+
+    const applicationsRepo = getRepository(Application)
+    const applications = await applicationsRepo
+      .createQueryBuilder('app')
+      .innerJoin('app.event', 'event')
+      .where('event.id = :eventId', { eventId })
+      .andWhere('app.approved = true')
+      .addOrderBy('app.groupCode', 'ASC')
+      .addOrderBy('app.surname', 'ASC')
+      .addOrderBy('app.firstname', 'ASC')
+      .addOrderBy('app.midname', 'ASC')
+      .getMany()
+
+    const users = applications.map((app) => app.represent())
+    await ctx.reply(users.join(', '))
+    await ctx.reply(users.join('\n'))
+
+    return ctx.scene.enter(SCENE.DEFAULT)
+  })
+)
